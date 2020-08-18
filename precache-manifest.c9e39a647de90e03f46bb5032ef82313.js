127509a8d2b8f22905a7590e4383cbca989b701d
hieunugent <hieunugent@gmail.com>
2020-08-17 19:55:56 -0700

    Updates

---
+++ precache-manifest.c9e39a647de90e03f46bb5032ef82313.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd846486ee9b4785f9e2ee2580bf940",
+    "url": "/coffeelovertully/index.html"
+  },
+  {
+    "revision": "7b5ba290e3652af678f9",
+    "url": "/coffeelovertully/static/css/main.df78a8c9.chunk.css"
+  },
+  {
+    "revision": "49753249f9f4ed6eb315",
+    "url": "/coffeelovertully/static/js/2.c60b430a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coffeelovertully/static/js/2.c60b430a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5ba290e3652af678f9",
+    "url": "/coffeelovertully/static/js/main.cd5886fa.chunk.js"
+  },
+  {
+    "revision": "4fa6f5d87872a0225786",
+    "url": "/coffeelovertully/static/js/runtime-main.4868a8ae.js"
+  },
+  {
+    "revision": "152c1f138c19671fede15545c8d2c28f",
+    "url": "/coffeelovertully/static/media/final2020.152c1f13.jpg"
+  },
+  {
+    "revision": "58e077fc5b99d9a1e4eb9a6b4896d7e0",
+    "url": "/coffeelovertully/static/media/final20202.58e077fc.jpg"
+  },
+  {
+    "revision": "e5489383c4f4f9e65ba4b703f4ac2cbd",
+    "url": "/coffeelovertully/static/media/final20203.e5489383.jpg"
+  },
+  {
+    "revision": "afa7e3cf758186360ddae84ece5bb434",
+    "url": "/coffeelovertully/static/media/final20204.afa7e3cf.jpg"
+  },
+  {
+    "revision": "b81fcd811d995da0ee95c222a623ee4c",
+    "url": "/coffeelovertully/static/media/final20205.b81fcd81.jpg"
+  },
+  {
+    "revision": "f856685336fd3666292d38f8faee01f9",
+    "url": "/coffeelovertully/static/media/final20206.f8566853.jpg"
+  },
+  {
+    "revision": "2aa4be8492ce186ed1079e1ec0b2a9ce",
+    "url": "/coffeelovertully/static/media/final20207.2aa4be84.jpg"
+  },
+  {
+    "revision": "9c3ed713ad9b38877fa5abfb3c302a6f",
+    "url": "/coffeelovertully/static/media/logo.9c3ed713.png"
+  }
+]);
